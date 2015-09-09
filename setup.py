68003a95c2bde7b26101a9621de814ff82a73b64
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-09-09 16:30:33 +0900

    Add bash/zsh completion

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from setuptools import setup
 
+import os
+import sys
 import imp
+import argparse
 
 
 def get_version():
@@ -15,10 +18,48 @@ def get_version():
             ver_file.close()
 
 
+def _resolve_prefix(prefix, type):
+    osx_system_prefix = '/System/Library/Frameworks/Python.framework/Versions'
+    if type == 'man':
+        if prefix == '/usr':
+            return '/usr/share'
+        if sys.prefix.startswith(osx_system_prefix):
+            return '/usr/share'
+    elif type == 'bash_comp':
+        if prefix == '/usr':
+            return '/'
+        if sys.prefix.startswith(osx_system_prefix):
+            return '/'
+    elif type == 'zsh_comp':
+        if sys.prefix.startswith(osx_system_prefix):
+            return '/usr'
+    else:
+        raise ValueError('not supported type')
+    return prefix
+
+
+def get_data_files(prefix):
+    data_files = []
+    bash_comp_dest = os.path.join(_resolve_prefix(prefix, 'bash_comp'),
+                                  'etc/bash_completion.d')
+    data_files.append((bash_comp_dest, ['completion/wstool-completion.bash']))
+    zsh_comp_dest = os.path.join(_resolve_prefix(prefix, 'zsh_comp'),
+                                 'share/zsh/site-functions')
+    data_files.append((zsh_comp_dest, ['completion/_wstool']))
+    return data_files
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--prefix', default='',
+                    help='prefix to install data files')
+opts, _ = parser.parse_known_args(sys.argv)
+prefix = opts.prefix
+
 setup(name='wstool',
       version=get_version(),
       packages=['wstool'],
       package_dir={'': 'src'},
+      data_files=get_data_files(prefix),
       # rosinstall dependency to be kept in order not to break ros hydro install instructions
       install_requires=['vcstools>=0.1.34', 'pyyaml'],
       scripts=["scripts/wstool"],
