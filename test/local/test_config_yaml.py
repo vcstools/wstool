1cc9785a98cd6117ba1babb01eaa96db6f0007ae
Ryan Sinnet <ryan@rwsinnet.com>
2016-11-28 23:48:04 -0800

    Replacing assertLess with assertTrue to support Python 2.6.

--- test/local/test_config_yaml.py
+++ test/local/test_config_yaml.py
@@ -194,7 +194,7 @@ class ConfigFile_Test(unittest.TestCase):
         with open(filepath, 'r') as f:
             read_data = f.read()
         lines = read_data.splitlines()
-        self.assertLess(0, len(lines))
+        self.assertTrue(len(lines) > 0)
         self.assertEqual(header, lines[0])
         check_config_entries(self, lines[1:], uuid)
 
